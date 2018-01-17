ca6d5a4ff744d666bdb5ea5713731c7b9e9a3ab7
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 22:44:39 +0000

    advanced 2 with new line!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
