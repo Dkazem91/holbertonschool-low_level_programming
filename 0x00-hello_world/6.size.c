af340292e396c07e821f0933d6b898e7bcd13ac4
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 21:59:19 +0000

    fixed 6

--- 0x00-hello_world/6.size.c
+++ 0x00-hello_world/6.size.c
@@ -7,4 +7,5 @@
 int main(void)
 {
 	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+	return(0);
 }
