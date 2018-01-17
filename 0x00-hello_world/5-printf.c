40c1acbff68ee65f4722196e58a08f03035fc02e
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 21:35:52 +0000

    trying 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
