db95ab73f536d43d0b0ae81294013d32edf1f1d3
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 22:18:58 +0000

    stupid C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	puts("Size of a char: " + sizeof(a) + " byte(s)");
-	puts("Size of an int: " + sizeof(b) + " byte(s)");
-	puts("Size of a long int: " + sizeof(c) + " byte(s)");
-	puts("Size of a long long int: " + sizeof(b) + " byte(s)");
-	puts("Size of a float: " + sizeof(b) + " byte(s)");
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
