6f70d1665b7ad2891103e9c49efd7eb2765609b9
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 23:56:37 +0000

    trying it again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
