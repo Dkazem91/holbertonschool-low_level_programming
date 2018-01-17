0a771e1979e95b6918d88b13dfd9dabc29fea410
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 21:24:39 +0000

    fixed formatting stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
