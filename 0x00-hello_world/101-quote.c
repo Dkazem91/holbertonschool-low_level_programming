5c8ad181fde8c3d1d005079f02c08644e69acfdc
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 22:39:11 +0000

    second advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19");
+	return (1);
+}
