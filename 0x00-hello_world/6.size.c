7b911754cb29bb7532199859e9ff0a3efc335887
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 21:56:29 +0000

    did 6

---
+++ 0x00-hello_world/6.size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+}
