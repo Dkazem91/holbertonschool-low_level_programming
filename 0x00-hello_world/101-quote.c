e31103bca6bdae4cd554d4bae98c9d7c75cf0ab2
Dkazem91 <Dkazemian@gmail.com>
2018-01-17 22:46:06 +0000

    fixed advanced again!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
