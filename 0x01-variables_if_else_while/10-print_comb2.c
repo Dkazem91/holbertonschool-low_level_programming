d8509a8a177d00d575f365b7c6056ea2f01d7112
Dkazem91 <Dkazemian@gmail.com>
2018-01-18 22:31:58 +0000

    done with 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 10; i <= 19; i++)
+	{
+		for (j = 10; j <= 19; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 19 ||  j != 19)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+	putchar('\n');
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 11; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
