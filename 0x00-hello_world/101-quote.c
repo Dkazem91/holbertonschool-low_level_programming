b788600b5170458420fb01493b508d5bc7cf004f
Dkazem91 <Dkazemian@gmail.com>
2018-01-18 01:09:05 +0000

    fixed advanced for real

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	fprintf(
-stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
