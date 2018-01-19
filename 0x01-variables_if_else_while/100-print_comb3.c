f3b37fbf6fcab1451f238e144076dda8462f183f
Dkazem91 <Dkazemian@gmail.com>
2018-01-19 00:30:53 +0000

    done with 1st advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int n = 0;
+
+	for (i = 10; i <= 19; i++)
+	{
+		for (j = 10; j <= 19; j++)
+		{
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
