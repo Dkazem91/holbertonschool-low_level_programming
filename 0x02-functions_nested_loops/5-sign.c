a1c250d9aa538d93a0bdaefb62b78feb49d5e750
Dkazem91 <Dkazemian@gmail.com>
2018-01-23 18:46:40 +0000

    fixed 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,12 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} else
-	{
-		_putchar('-');
-		return (-1);
 	}
+	_putchar('-');
+	return (-1);
 }
