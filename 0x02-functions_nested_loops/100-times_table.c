ea45c8fb13d5af9934c3ab059e46b5d4204adce5
Dkazem91 <Dkazemian@gmail.com>
2018-01-23 20:43:10 +0000

    fixed first advanced with parantheses

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
+					_putchar(((prod / 10) % 10) + '0');
 					_putchar((prod % 10) + '0');
 				} else
 				{
@@ -44,6 +44,7 @@ void print_times_table(int n)
 					_putchar((prod % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
