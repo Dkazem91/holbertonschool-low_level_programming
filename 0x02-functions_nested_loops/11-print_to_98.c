c39bc18c4c298b697f9e40d03b75956b9a29886e
Dkazem91 <Dkazemian@gmail.com>
2018-01-23 20:23:28 +0000

    Fixed last mandatory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,11 +5,13 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n > 98){
+
+	if (n > 98)
+	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d",i);
-			if(i != 98)
+			printf("%d", i);
+			if (i != 98)
 			{
 				printf(", ");
 			}
@@ -18,7 +20,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d",i);
+			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
