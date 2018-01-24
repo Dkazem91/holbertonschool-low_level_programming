7f0356c8c8660812991147e2b575f54ac93b3475
Dkazem91 <Dkazemian@gmail.com>
2018-01-24 00:43:51 +0000

    trying last advanced effor again

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,11 +12,12 @@ int main(void)
 	double b = a + 1;
 	double c = a + b;
 
-	printf("%.0f, %.0f, ", a, b);
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
 	while (counter < 98)
 	{
-		printf("%.0f", c);
 		counter++;
+		printf("%.0f", c);
 		a = b;
 		b = c;
 		c = a + b;
