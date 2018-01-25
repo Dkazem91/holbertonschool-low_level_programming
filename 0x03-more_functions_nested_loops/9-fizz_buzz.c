8f284d24f85fd95c0b811ad98287565006d6caf3
Dkazem91 <Dkazemian@gmail.com>
2018-01-25 23:02:19 +0000

    fixed warnings on fizzbuzz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,26 @@
 int main(void)
 {
 	int i;
-	int result;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
+			{
 				printf("Fizz");
+			}
 			if (i % 5 == 0)
+			{
 				printf("Buzz");
+			}
 		} else
+		{
 			printf("%d", i);
+		}
 		if (i != 100)
 			printf(" ");
 	}
 	printf("\n");
+	return (0);
 }
