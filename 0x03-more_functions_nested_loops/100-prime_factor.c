a14b3b789fb630da11a1e63f5204302bf606898f
Dkazem91 <Dkazemian@gmail.com>
2018-01-26 00:27:47 +0000

    first advanced

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	unsigned int a = 2;
+	unsigned long n = 612852475143;
+
+	while ( a != n)
+	{
+		if(n % a ==0)
+		{
+			n = n / a;
+		}else
+		{
+			a++;
+		}
+	}
+	printf("%lu\n",n);
+	return (0);
+}
