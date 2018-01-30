8984d8fd1d5224ac74c7f18f1d852280f60efb1a
Dkazem91 <Dkazemian@gmail.com>
2018-01-30 20:37:46 +0000

    moved printf into loop

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (i != n -1)
+			printf(", ");
 	}
-	printf("%d\n", a[i]);
+	printf("\n");
 }
