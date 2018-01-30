c0f4f7f5dd8b24c0cd3e84064b27437f4ccba954
Dkazem91 <Dkazemian@gmail.com>
2018-01-30 20:16:33 +0000

    fixed betty and commas

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n -1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d ", a[i]);
+		printf("%d, ", a[i]);
 	}
 	printf("%d\n", a[i]);
 }
