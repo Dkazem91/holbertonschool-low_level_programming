29e70dac08bf7a963b83b6cc23b33c217ac13d3a
Dkazem91 <Dkazemian@gmail.com>
2018-01-31 01:00:34 +0000

    fixed

---
+++ 0x04-pointers_arrays_strings/8-main.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[6];
+
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	array[5] = -200003;
+	print_array(array, 6);
+	return (0);
+}
--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -16,5 +16,5 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
