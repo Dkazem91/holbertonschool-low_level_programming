b9748374212506ba23b20962e5e275e97e1bd5d5
Dkazem91 <Dkazemian@gmail.com>
2018-02-06 02:03:14 +0000

    fixed 5 i think

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -12,12 +12,15 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != haystack[i] && needle[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-		}
-		if (needle[j - 1] == '\0')
-		{
-			return (haystack + i);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
 		}
 
 	}
