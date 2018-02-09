05c05747e3acc04485b0a78c6189aedccc5a36ee
Dkazem91 <Dkazemian@gmail.com>
2018-02-09 23:47:13 +0000

    done with 0

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
 /**
- *
- *
- *
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
-int main(void)
+int main(int argc, char* argv[])
 {
+	UNUSED(argc);
+	printf("%s\n",argv[0]);
+	return (0);
 }
