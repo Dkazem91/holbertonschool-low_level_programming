c36c1b19d39fbbab335bf44148a4f435b9bfd2ab
Dkazem91 <Dkazemian@gmail.com>
2018-02-11 00:15:03 +0000

    did advanced

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	int coins = 0;
+
+	if (argc == 2)
+	{
+		int money = atoi(argv[argc-1]);
+
+		if (money < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+				coins++;
+				continue;
+			}
+			if (money % 10 == 0)
+			{
+				money -= 10;
+				coins++;
+				continue;
+			}
+			if (money % 5 == 0)
+			{
+				money -= 5;
+				coins++;
+				continue;
+			}
+			money--;
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (1);
+	}
+	printf("Error\n");
+	return (1);
+}
---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define UNUSED(x) (void)(x)
+/**
+ * StringCheck - checks string
+ * @s: string to check
+ * Return: boolean
+ */
+int StringCheck(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char  *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (StringCheck(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+
+}
