b6e5ca09e7ab620437a69f7e51e3c68908006820
Dkazem91 <Dkazemian@gmail.com>
2018-02-12 08:01:27 +0000

    fixed advanced i think

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * main - main function
  * @argc: argumentc
@@ -13,13 +14,13 @@ int main(int argc, char  *argv[])
 
 	if (argc == 2)
 	{
-		int money = atoi(argv[argc - 1]);
-
-		if (money < 0)
+		if (strstr(argv[argc - 1], "-"))
 		{
 			printf("0\n");
 			return (1);
 		}
+
+		int money = atoi(argv[argc - 1]);
 
 		while (money > 0)
 		{
