4816977928253b9a31a16cdc2af6f2f6b70d3c6e
Dkazem91 <Dkazemian@gmail.com>
2018-02-13 23:30:17 +0000

    attempting fix at 3 again

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -9,16 +9,21 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int **t;
 	int i;
 	int j;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	int **t = (int **)malloc(sizeof(int *) * height);
+	 t = (int **)malloc(sizeof(int *) * height);
 
 	for (i = 0; i < width; i++)
+	{
 		t[i] = (int *)malloc(sizeof(int) * width);
+		if (t[i] == NULL)
+			return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
