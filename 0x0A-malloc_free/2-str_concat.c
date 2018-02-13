e0773300cff98ebeb48803cec875737f9dd9d070
Dkazem91 <Dkazemian@gmail.com>
2018-02-13 22:43:59 +0000

    fixing 2?

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -3,47 +3,51 @@
 #include <stdlib.h>
 /**
  * str_concat - concats strings
- * @s1: first string to concat
- * @s2: second string to concat
- * Return: char value of concated string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: returns concated string
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i, len1, len2;
+	char *conc;
+
+	i = 0;
+	len1 = 0;
+	len2 = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	int len1;
-	int len2;
-	int i;
-	char *t;
-
-	i = 0;
-	len1 = 0;
-	len2 = 0;
-
-	while (s1[i] != '\0')
+	while(s1[i] != '\0')
 	{
-		len1++;
 		i++;
+		len1++;
 	}
+
 	i = 0;
+
 	while (s2[i] != '\0')
 	{
-		len2++;
 		i++;
+		len2++;
 	}
 
-	t = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (t == NULL)
+	conc = malloc(sizeof(char) * (len1 + len2 +1));
+	if (conc == NULL)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
-		t[i] = s1[i];
+	{
+		conc[i] = s1[i];
+	}
 
 	for (i = 0; i < len2; i++)
-		t[i + len1] = s2[i];
+	{
+		conc[i + len1] = s2[i];
+	}
+	conc[i + len1] = '\0';
 
-	return (t);
+	return (conc);
 }
