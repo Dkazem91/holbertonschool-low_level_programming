4ae24865510ca997c099d1dd5f1b7fc9ac4f3c8f
Dkazem91 <Dkazemian@gmail.com>
2018-02-15 23:02:10 +0000

    fixed 2 null cases

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -12,7 +12,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *t;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	t = malloc(nmemb * size);
+	if (t == NULL)
+		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
 		t[i] = 0;
 
