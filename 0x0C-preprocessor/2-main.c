d8f954214adf88636faa5a9d77c87422b3db20bb
Dkazem91 <Dkazemian@gmail.com>
2018-02-19 21:17:36 +0000

    added main file

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
