c1526bdd6fee0ad2ebd441945153ae0bd4707adb
Dkazem91 <Dkazemian@gmail.com>
2018-02-19 23:24:52 +0000

    fixed betty one 2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,17 +8,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
-		if(d->name == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if(d->age <= 0)
+		if (d->age <= 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
-		if(d->owner == NULL)
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
