4265680289a5c6a47f24cfd627e5b396992008dd
Dkazem91 <Dkazemian@gmail.com>
2018-02-19 23:57:06 +0000

    took away crap

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,42 +10,14 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int len1, len2, i;
-	char *a, *b;
 	struct dog *ndog;
-
-	i = 0;
-	while (name[i] != '\0')
-	{
-		len1++;
-		i++;
-	}
-	while (owner[i] != '\0')
-	{
-		len2++;
-		i++;
-	}
-	a = malloc(sizeof(char) * (len1 + 1));
-	if (a == NULL)
-		return (NULL);
-	b = malloc(sizeof(char) * (len2 + 1));
-	if (b == NULL)
-		return (NULL);
-
-	for (i = 0; i < len1; i++)
-		a[i] = name[i];
-	a[i + 1] = '\0';
-
-	for (i = 0; i < len2; i++)
-		b[i] = owner[i];
-	b[i + 1] = '\0';
 
 	ndog = malloc(sizeof(struct dog));
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = a;
+	ndog->name = name;
 	ndog->age = age;
-	ndog->owner = b;
+	ndog->owner = owner;
 
 
 	return (ndog);
