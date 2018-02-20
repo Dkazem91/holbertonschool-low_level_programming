42711f1c7c7bed8cb9e3e8fd547bf8bca61fdccc
Dkazem91 <Dkazemian@gmail.com>
2018-02-20 01:28:33 +0000

    Fixed null check for 1

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
