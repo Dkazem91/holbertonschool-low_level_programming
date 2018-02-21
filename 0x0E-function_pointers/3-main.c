b6b0542a458647c5488ac1826ea3e62a3ac45795
Dkazem91 <Dkazemian@gmail.com>
2018-02-21 23:37:19 +0000

    added main

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: arguments
+ * @argv: array of args
+ * Return: return something
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		int a;
+		int b;
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (1);
+	}
+	printf("Error\n");
+	exit(98);
+}
