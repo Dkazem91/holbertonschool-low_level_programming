ab9a06c5d96fa0d6e417fdc7d55762ab06ac64fd
Dkazem91 <Dkazemian@gmail.com>
2018-02-21 23:59:10 +0000

    fixed 1

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || action == NULL)
+		exit(98);
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
