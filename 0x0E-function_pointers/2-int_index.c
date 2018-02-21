928df7cda5d3efcc432a8552c493eb7180ef4006
Dkazem91 <Dkazemian@gmail.com>
2018-02-21 23:54:36 +0000

    fixed 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
