f21b8532a536d93fd4c0ff3a3a00225abd4fdc4c
Dkazem91 <Dkazemian@gmail.com>
2018-03-01 22:28:35 +0000

    fixed betty for 1

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * list_len - length of list_t struct
+ * @h: head node
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
