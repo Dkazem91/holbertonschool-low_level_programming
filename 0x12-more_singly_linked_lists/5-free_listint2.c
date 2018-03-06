8f05c755fdb791b321635dbf317f821c90894ba1
Dkazem91 <Dkazemian@gmail.com>
2018-03-06 19:51:27 +0000

    added null check

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		tmp = *head;
 		free(*head);
 		*head = tmp->next;
 	}
-
+	*head = NULL;
 }
