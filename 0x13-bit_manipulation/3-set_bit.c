d6067aabd5a39ad3e96489008c444ce630882edc
Dkazem91 <Dkazemian@gmail.com>
2018-03-08 22:39:24 +0000

    fixed return values

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include "2-get_bit.c"
 /**
  *
  *
@@ -6,5 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	return ((*n) |= 1 << index);
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
 }
