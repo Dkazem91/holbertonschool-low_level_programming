2c8d215542229c470ed080c0c8fec8a44ccdc25c
Dkazem91 <Dkazemian@gmail.com>
2018-03-08 22:09:48 +0000

    fixed bettyfor 0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * binary_to_uint - binary to uint
+ * @b: string to convert
+ * Return: unsigned int
  *
  */
 unsigned int binary_to_uint(const char *b)
@@ -15,10 +15,10 @@ unsigned int binary_to_uint(const char *b)
 	{
 		count++;
 	}
-	for (i = count-1; i != 0; i--)
+	for (i = count - 1; i != 0; i--)
 	{
 		if (b[i] != '1' && b[i] != '0')
-			return (0);
+			return (sum * 0);
 		sum += (b[i] - '0') * bi;
 		bi *= 2;
 	}
