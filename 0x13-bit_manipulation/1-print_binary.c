6530fea30366b7eaa4a2600bb3e6e2e67ffa9980
Dkazem91 <Dkazemian@gmail.com>
2018-03-08 23:47:51 +0000

    fixed size of

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -5,16 +5,33 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long long i = n;
+	unsigned long int i = 1;
 
+	i <<= ((sizeof(i) * 8) - 1);
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (i > 0)
+	{
+		if ((i & n) == 0)
+			i = i >> 1;
+		else
+			break;
+	}
 	while (i > 0)
 	{
 		if ((i & n) == 0)
 			_putchar('0');
 		else
-		{
 			_putchar('1');
-		}
 		i = i >> 1;
 	}
+
 }
