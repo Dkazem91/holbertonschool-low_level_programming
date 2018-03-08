09ac5da59eb0e50eff9c2e164002f83943ad28f1
Dkazem91 <Dkazemian@gmail.com>
2018-03-08 22:46:21 +0000

    fixed index check

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * get_bit - gets bit a index
+ * @n: number to go through
+ * @index: index to go through
+ * Return: int value
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 32)
+		return (-1);
+
 	return (n >> index & 1);
 }
