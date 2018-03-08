5907c265d84e830d97e02c7475eaca6b93211fe1
Dkazem91 <Dkazemian@gmail.com>
2018-03-08 22:50:44 +0000

    fixed 4

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
+#include "2-get_bit.c"
 /**
- *
- *
- *
- *
+ * clear_bit - clears index to 0
+ * @n: integer to pass
+ * @index: index to go to
+ * Return: returns integer val
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return ((*n) 
+	if (index > 32)
+		return (-1);
+
+	(*n) &= ~(1 << index);
+
+	if (get_bit((*n), index) == 0)
+		return (1);
+	return (-1);
 }
