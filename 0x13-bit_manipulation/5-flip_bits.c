91097a7e6c89c174d7d73544c34f42625f50663c
Dkazem91 <Dkazemian@gmail.com>
2018-03-09 01:04:01 +0000

    it works

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -7,7 +7,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flips, count = 0;
+	unsigned long int flips, count = 0;
 
 	flips = n ^ m;
 	while (flips > 0)
