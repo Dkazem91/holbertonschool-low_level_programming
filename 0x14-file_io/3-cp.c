f8b473d8cacfb4d91a62353bb3647bf2fa71ff51
Dkazem91 <Dkazemian@gmail.com>
2018-03-13 22:48:48 +0000

    fixed 3 i think

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,15 +15,17 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		free(buff)
 		exit(97);
 	}
-	fdFrum = open(av[1], O_RDWR);
+	fdFrum = open(av[1], O_RDONLY);
 	if (fdFrum == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		free(buff);
 		exit(98);
 	}
-	fdToo = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((readed = read(fdFrum, buff, 1024)) > 0)
 	{
 		wrote = write(fdToo, buff, readed);
@@ -34,13 +36,15 @@ int main(int ac, char **av)
 	if (close(fdFrum) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[1]);
+		free(buff);
 		exit(100);
 	}
 	if (close(fdToo) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", av[2]);
+		free(buff);
 		exit(100);
 	}
 	free(buff);
-	return (1);
+	return (0);
 }
