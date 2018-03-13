d1adba051abdf7cee259ec54ecc0dc60ea10fa11
Dkazem91 <Dkazemian@gmail.com>
2018-03-13 21:45:12 +0000

    fixed 0 and 3 betty at least

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * read_textfile - reads a textfile
+ * @filename: file to read
+ * @letters: amount of bytes to read
+ * Return: printed letters count
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,16 +13,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename,O_RDWR);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
-	printed = read(fd,buff,letters);
+	printed = read(fd, buff, letters);
 	if (printed == -1)
 		return (0);
-	wrote = write(1,buff,printed);
+	wrote = write(1, buff, printed);
 	if (wrote == -1)
 		return (0);
-	if(close(fd) == -1)
+	if (close(fd) == -1)
 		return (0);
 	return (printed);
 }
--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,45 +1,44 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
- *
+ * main - program to copy
+ * @ac: argument count
+ * @av: array of arguments
+ * Return: a value
  */
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
-	int fdFrum,fdToo;
-	long int wrote,readed;
+	int fdFrum, fdToo;
+	long int wrote, readed;
 	char buff[1024];
 
 	if (ac != 3)
 	{
-		dprintf(2,"Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdFrum = open(av[1],O_RDWR);
+	fdFrum = open(av[1], O_RDWR);
 	if (fdFrum == -1)
 	{
-		dprintf(2,"Error: Can't read from file %s\n", av[1]);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fdToo = open(av[2],O_CREAT | O_RDWR | O_TRUNC,0664);
-	while ((readed = read(fdFrum,buff,1024)) > 0)
+	fdToo = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	while ((readed = read(fdFrum, buff, 1024)) > 0)
 	{
-		wrote = write(fdToo,buff,readed);
+		wrote = write(fdToo, buff, readed);
 		if (wrote == -1)
-			return(-1);
+			return (-1);
 	}
 
-	if(close(fdFrum))
+	if (close(fdFrum) == -1)
 	{
-		dprintf(2,"Error: Can't close fd %s\n",av[1]);
+		dprintf(2, "Error: Can't close fd %s\n", av[1]);
 		exit(100);
 	}
-	if(close(fdToo))
+	if (close(fdToo) == -1)
 	{
-		dprintf(2,"Error: Can't close fd %s\n",av[2]);
+		dprintf(2, "Error: Can't close fd %s\n", av[2]);
 		exit(100);
 	}
-	return(1);
+	return (1);
 }
