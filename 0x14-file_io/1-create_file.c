8585e15087ef19006a3c11244ab9c23933d2a387
Dkazem91 <Dkazemian@gmail.com>
2018-03-13 22:26:03 +0000

    fixed 1 bullshit!

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,8 +10,6 @@ int create_file(const char *filename, char *text_content)
 	int fd, len = 0;
 	long int wrote;
 
-	while (text_content[len])
-		len++;
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -19,10 +17,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
+
+		while (text_content[len])
+			len++;
 		wrote = write(fd, text_content, len);
 		if (wrote == -1)
 			return (-1);
 	}
-	close(fd);
+	if(close(fd) == -1)
+		return (-1);
 	return (1);
 }
