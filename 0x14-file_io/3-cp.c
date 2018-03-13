a6114d48f32e781a8676dbfab7ff82d8d163fd59
Dkazem91 <Dkazemian@gmail.com>
2018-03-13 21:29:05 +0000

    deleted some stuff, did 3

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main (int ac, char **av)
+{
+	int fdFrum,fdToo;
+	long int wrote,readed;
+	char buff[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fdFrum = open(av[1],O_RDWR);
+	if (fdFrum == -1)
+	{
+		dprintf(2,"Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fdToo = open(av[2],O_CREAT | O_RDWR | O_TRUNC,0664);
+	while ((readed = read(fdFrum,buff,1024)) > 0)
+	{
+		wrote = write(fdToo,buff,readed);
+		if (wrote == -1)
+			return(-1);
+	}
+
+	close(fdFrum);
+	if (close == -1)
+	{
+		dprintf(2,"Error: Can't close fd %s\n",av[1]);
+		exit(100);
+	}
+	close(fdToo);
+	if (close == -1)
+	{
+		dprintf(2,"Error: Can't close fd %s\n",av[2]);
+		exit(100);
+	}
+	return(1);
+}
