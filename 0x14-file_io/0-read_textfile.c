58843c776a2878aaadbca4341bd4d80599bc3c67
Dkazem91 <Dkazemian@gmail.com>
2018-03-13 22:03:14 +0000

    changed buffer to malloc bullshit

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -8,8 +8,11 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, printed, wrote;
-	char buff[letters];
+	char *buff;
 
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDWR);
@@ -23,5 +26,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	if (close(fd) == -1)
 		return (0);
+	free(buff);
 	return (printed);
 }
