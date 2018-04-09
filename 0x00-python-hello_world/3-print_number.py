d2645c45f8300180fb970e9292c74219b52ad67d
Dkazem91 <Dkazemian@gmail.com>
2018-04-09 20:25:43 +0000

    added python answers

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 98
+print ("{:d} Battery street".format(number))
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print("{}{}{}".format(str,str,str))
+print("{}".format(str[:9]))
