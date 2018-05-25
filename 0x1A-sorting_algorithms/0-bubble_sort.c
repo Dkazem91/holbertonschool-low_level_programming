3acd27d3fe2524e867a2464f84f9b8cf091d8a5c
Dkazem91 <Dkazemian@gmail.com>
2018-05-25 10:45:49 +0000

    cleaned up repo

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * bubble_sort sorts bubbly
+ * bubble_sort - sorts bubbly
  * @array: array to sort
  * @size: size
  *
@@ -10,9 +10,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	int tmp, swaps = 0;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if(array[i] > array[i + 1])
+		if (array[i] > array[i + 1])
 		{
 			tmp = array[i + 1];
 			array[i + 1] = array[i];
@@ -21,6 +21,6 @@ void bubble_sort(int *array, size_t size)
 			swaps = 1;
 		}
 	}
-	if(swaps)
+	if (swaps)
 		bubble_sort(array, size);
 }
