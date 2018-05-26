cae42d3f46cf2789b44e3ea5f0d004a95626c06d
Dkazem91 <Dkazemian@gmail.com>
2018-05-26 16:41:44 +0000

    added quick sort

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+/**
+ * partition - partitions the array
+ * @array: array to take in
+ * @start: start of array;
+ * @end: end of array
+ * @size: full size of array
+ * Return: position of pivot
+ */
+int partition(int *array, int start, int end, int size)
+{
+	(void)size;
+	int pivot = array[end];
+	int i = start, j, temp;
+
+	for (j = start; j < end; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	temp = array[i];
+	array[i] = array[end];
+	array[end] = temp;
+	if (i != end)
+		print_array(array, size);
+	return (i);
+}
+/**
+ * quickSort - quick sorts with recursion
+ * @array: array to sort through
+ * @start: start of array or subarray
+ * @end: end of array or subarray
+ * @size: size of full array
+ */
+void quickSort(int *array, int start, int end, int size)
+{
+	int pivot;
+
+	if (start < end)
+	{
+		pivot = partition(array, start, end, size);
+		quickSort(array, start, pivot - 1, size);
+		quickSort(array, pivot + 1, end, size);
+	}
+}
+/**
+ * quick_sort - quick sorts an array
+ * @array: array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	quickSort(array, 0, size - 1, size);
+}
--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -24,4 +24,5 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
