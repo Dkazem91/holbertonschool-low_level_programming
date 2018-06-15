6a49a2f92b6eb965f846ea977b34e362302ab7be
Dkazem91 <Dkazemian@gmail.com>
2018-06-15 14:03:46 +0000

    added extra return in try:except

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@ def checkdirection(array, idx):
     try:
         if not array[idx]:
             return 1
+        return 0
     except:
         return 0
 def island_perimeter(grid):
@@ -12,8 +13,8 @@ def island_perimeter(grid):
     for idx1, row in enumerate(grid):
         for idx2, spot in enumerate(row):
             if spot:
-                count += checkdirection(grid[idx1 - 1], idx2)
-                count += checkdirection(row, idx2 - 1)
-                count += checkdirection(grid[idx1 + 1], idx2)
-                count += checkdirection(row, idx2 + 1)
+                count = count + checkdirection(grid[idx1 - 1], idx2)
+                count = count + checkdirection(row, idx2 - 1)
+                count = count + checkdirection(grid[idx1 + 1], idx2)
+                count = count + checkdirection(row, idx2 + 1)
     return count
