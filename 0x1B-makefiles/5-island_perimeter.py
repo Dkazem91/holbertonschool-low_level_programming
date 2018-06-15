57a8caa42ce4eb0b34be6ed0d396530f48626ee4
Dkazem91 <Dkazemian@gmail.com>
2018-06-15 15:42:50 +0000

    switched to a bunch of if else statements

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -2,31 +2,33 @@
 """gets perimeter of island"""
 
 
-def checkdirection(array, idx):
-    """checks the direction"""
-    try:
-        if not array[idx]:
-            return 1
-        return 0
-    except:
-        return 1
-
-
 def island_perimeter(grid):
     """actual function"""
     count = 0
-    if (grid and isinstance(grid, list) and
-        all(isinstance(row, list) for row in grid)
-        and all([[isinstance(element, int) for element in row]
-                 for row in grid])):
+    if grid:
+        glen = len(grid)
+        rlen = len(grid[0])
         for idx1, row in enumerate(grid):
             for idx2, spot in enumerate(row):
                 if spot:
-                    count = count + checkdirection(grid[idx1 - 1], idx2)
-                    count = count + checkdirection(row, idx2 - 1)
-                    count = count + checkdirection(row, idx2 + 1)
-                    if idx1 < len(grid) - 1:
-                        count = count + checkdirection(grid[idx1 + 1], idx2)
-                    else:
+                    if idx1 == 0:
                         count += 1
+                    else:
+                        if not grid[idx1 - 1][idx2]:
+                            count += 1
+                    if idx1 >= glen - 1:
+                        count += 1
+                    else:
+                        if not grid[idx1 + 1][idx2]:
+                            count += 1
+                    if idx2 == 0:
+                        count += 1
+                    else:
+                        if not row[idx2 - 1]:
+                            count += 1
+                    if idx2 >= rlen - 1:
+                        count += 1
+                    else:
+                        if not row[idx2 + 1]:
+                            count += 1
     return count
