50e114af426f8ee3882c33b5449a6d59b8fb79ff
Dkazem91 <Dkazemian@gmail.com>
2018-07-02 11:12:18 +0000

    added new directory with readmes and print binary tree function

---
+++ 0x1C-binary_trees/binary_trees.h
@@ -0,0 +1,27 @@
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
+#endif
