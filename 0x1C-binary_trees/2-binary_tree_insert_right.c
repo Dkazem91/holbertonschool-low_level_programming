cc2c9eedbf9afa90ff6e3eaa8c8e7f24c9bf6eda
Dkazem91 <Dkazemian@gmail.com>
2018-07-05 13:34:26 +0000

    fixed styling

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -8,7 +8,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	if(!(parent))
+	if (!(parent))
 		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (!(new))
