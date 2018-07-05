005820d1c8bcc5423455f98a99e22c061a9bc0f1
Dkazem91 <Dkazemian@gmail.com>
2018-07-05 13:31:02 +0000

    seperated null checks

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -9,8 +9,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (!(parent))
+		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
-	if (!(new) || !(parent))
+	if (!(new))
 		return (NULL);
 	new->n = value;
 	new->parent = parent;
