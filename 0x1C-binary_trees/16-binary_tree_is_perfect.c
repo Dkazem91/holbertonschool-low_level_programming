cb9f8c83c31b23a2154f6518534768e090309efb
Dkazem91 <Dkazemian@gmail.com>
2018-07-05 14:16:23 +0000

    removed includes, changed height a little to check both sides are null

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,6 +1,57 @@
 #include "binary_trees.h"
-#include "15-binary_tree_is_full.c"
-#include "14-binary_tree_balance.c"
+/**
+ * binary_tree_is_full - finds if tree is full or not
+ * @tree: root node
+ * Return: balance
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
+}
+/**
+ * height - returns height
+ * @tree: node to start
+ * Return: height
+ */
+size_t height(const binary_tree_t *tree)
+{
+	size_t lheight, rheight;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	lheight = height(tree->left);
+	rheight = height(tree->right);
+
+	if (lheight > rheight)
+		return (lheight + 1);
+	return (rheight + 1);
+}
+/**
+ * binary_tree_balance - finds balance of tree at root
+ * @tree: root node
+ * Return: balance
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int lside, rside, balance;
+
+	if (tree == NULL)
+		return (0);
+	lside = height(tree->left);
+	rside = height(tree->right);
+	balance = lside - rside;
+	return (balance);
+}
+
 /**
  * binary_tree_is_perfect - finds if tree is full or not
  * @tree: root node
@@ -8,9 +59,11 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL || tree->parent != NULL)
+	if (tree == NULL)
 		return (0);
-	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
+
+	if (binary_tree_is_full(tree) && !(binary_tree_balance(tree)))
 		return (1);
+
 	return (0);
 }
