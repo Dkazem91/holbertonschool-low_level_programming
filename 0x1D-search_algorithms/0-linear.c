5dfd469cb5c0de0f81dbe264f462e8499a97c428
Dkazem91 <Dkazemian@gmail.com>
2018-08-07 21:54:50 +0000

    removed extra files

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches linear
+ * @array: array to search
+ * @size: size to go through
+ * @value: value to search?
+ * Return: int value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%zd] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
