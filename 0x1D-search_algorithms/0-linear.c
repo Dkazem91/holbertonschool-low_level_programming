8b74986bdfd42be3d570f49fb2272eec39992366
Dkazem91 <Dkazemian@gmail.com>
2018-08-09 20:05:19 +0000

    changed header include and printf type

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%zd] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -2,7 +2,6 @@
 #define _SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
