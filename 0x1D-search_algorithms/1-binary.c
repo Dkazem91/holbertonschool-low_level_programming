e1634c1ab270e17abdd85312913c24a5e86ef921
Dkazem91 <Dkazemian@gmail.com>
2018-08-09 20:24:56 +0000

    changed null check to == NULL

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -53,7 +53,7 @@ int actual_search(int *array, size_t l, size_t h, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (!(array))
+	if (array == NULL)
 		return (-1);
 
 	return (actual_search(array, 0, size - 1, value));
